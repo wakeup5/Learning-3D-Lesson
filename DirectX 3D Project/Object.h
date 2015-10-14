4b0a77411987284428ad56b52e90e574ed2d56d5
Jin Han Park <wakeup5@outlook.kr>
2015-10-14 05:19:19 +0000

    Imported from Changeset 404

--- DirectX 3D Project/Object.h
+++ DirectX 3D Project/Object.h
@@ -9,7 +9,12 @@ namespace NS_ROOT
 			UINT _ref;
 		public:
 			Object(){}
-			virtual ~Object(){}
+			virtual ~Object() 
+			{
+#ifdef _DEBUG
+				assert(_ref != 0 || "정상적으로 종료되지 않은 Object가 있습니다.");
+#endif
+			}
 
 			void AddRef(){ ++_ref; }
 			void Release() { if (!--_ref) delete this; }
